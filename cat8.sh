41c3d1c1337285170fec56b35d41ba30e7fe27fd
vishalbagde <vishalbagde.mmm@gmail.com>
2019-12-05 13:50:52 +0530

    modify cat8.sh file

--- cat8.sh
+++ cat8.sh
@@ -12,6 +12,7 @@ echo "add line from local"
 commit 2 from local server
 commit 3 from local server
 commit 4 from local server
+commit 5 from local server
 
 commit 1 from online server
 commit 2 from online server
